1fa4dc185271e011a703056d48f57c9e91d05f80
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2023-12-03 00:33:08 -0700

    Created script to change Title of Windows video files to its File Name

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # cpFileNameToTitle
-This is a python script that will copy the file name of a video file and apply it to the title in the meta data.
+This is a python script is intended for Windows systems. This script will copy the file name of a video file and apply it to the title in the meta data.
+
+# Purpose
+My family backs up all our dvds on a windows machine and we use that drive to stream all those videos to our smart devices in the house. One exception is our main tv room where there is an apple tv. I could run iTunes on the windows machine and share the backup drive with the apple tv. This would eliminate the rest of the family having to ask us to put movies on for them. There was one problem: iTunes displays the title of the file from the metadata and not the file name. This script was created to automate the task of copying the file name of each video file and pasting it into the 'title' metadata of the file.
+
+# Setup
+This script repires the **mutagen** and **pywin32** libraries. Yo ucan install them by typing the following into your command prompt:
+```
+pip install mutagen
+pip install pywin32
+```
+Once those dependencies are installed, navigate to the parent directory of the script and run it by entering into the command line:
+```
+python cpFileNameToTitle.py
+```
+You will be prompted for the URL of the folder containing the video file(s) you want to change:
+```
+Enter your media folders path: C:\Your\Video\Folder
+```
+Once entered, the script will crawl through the folder and all subfolders and change the title in the metadata from whatever it is to its file name. If the title attribute is missing from the file, this will add the attribute with the value of "1" and then change it to the desired value.
+
+**NOTE:** If there are any other types of files in the directory or any of the subdirectories, the script will terminate. This will be fixed with future iterations.
---
+++ cpFileNameToTitle.py
@@ -0,0 +1,50 @@
+import os
+import win32com.client
+from mutagen.easymp4 import EasyMP4
+
+	# This line starts the definition of a function named get_file_details that takes a parameter file_path.
+def get_file_details(file_path, new_title):
+	# Here, we create an instance of the Windows Shell Application using win32com.client.Dispatch. This allows us to interact with the Windows Shell.
+	shell = win32com.client.Dispatch("Shell.Application")
+	# We obtain a reference to the folder containing the file using shell.Namespace. os.path.dirname(file_path) extracts the directory path from the full file path.
+	folder = shell.Namespace(os.path.dirname(file_path))
+	# This line extracts the file name from the full file path using os.path.basename.
+	file_name = os.path.basename(file_path)
+	for i in range(folder.Items().Count):
+		file = folder.Items().Item(i)
+		# Load the metadata of the video file
+		video = EasyMP4(file_path)
+		keys = video.keys()
+		print("Available keys:", keys)
+		if len(keys) < 1:
+			print("There are no keys available. Adding key...")
+			# Create a new dictionary with the existing metadata
+			new_metadata = dict(video)
+			# Add the new key-value pair
+			new_metadata['title'] = '1'
+			# Update the existing metadata with the new dictionary
+			video.update(new_metadata)
+			print(f'"title" has been added to keys...')
+		else:
+			print("Keys available...")
+		title = video['title']
+		print(f"Title Before: {title}")
+		# Change the title
+		video['title'] = new_title
+		# Save the changes
+		video.save()
+		print(f"Title After: {video['title']}")
+		return title
+
+
+
+def main(folder_path):
+	for root, dirs, files in os.walk(folder_path):
+		for file in files:
+			file_path = os.path.join(root, file)
+			new_title = file.rstrip(".mp4")
+			title = get_file_details(file_path, new_title)
+
+if __name__ == "__main__":
+	folder_path = input("Enter your media folder's path: ")  # Change this to the path of your folder
+	main(folder_path)
